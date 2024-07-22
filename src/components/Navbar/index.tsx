5c5dbf4e3a8cc9ff3a02785560bb9eea501ea480
Fernando Lopez <fer.ragnar19@gmail.com>
2024-07-22 14:56:21 -0300

    fixs

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,15 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 const Navbar = () => {
   const { data: session, status }: any = useSession();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
   return (
     <nav className="bg-gray-800 p-4">
@@ -13,11 +19,8 @@ const Navbar = () => {
             src="/vagon_5_Transparent.png"
             alt="Logo"
             width={77}
-            height={0}
+            height={40}
           />
-          {/* <Link className="text-white font-bold text-xl" href="/">
-            Home
-          </Link> */}
         </div>
         <div className="flex space-x-4 items-center">
           {status === "authenticated" ? (
@@ -54,34 +57,48 @@ const Navbar = () => {
                   </Link>
                 </>
               )}
-              <span className="text-white">Hello, {session.user.email}</span>
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  signOut({
-                    redirect: true,
-                    callbackUrl: process.env.NEXT_PUBLIC_FRONTEND_BASE_URL,
-                  });
-                }}
-                className="text-white bg-red-500 px-3 py-2 rounded-md"
-              >
-                <a
-                  style={{ textDecoration: "none", color: "#fff" }}
-                  href={`/api/auth/signout`}
+              <div className="relative">
+                <button
+                  onClick={toggleDropdown}
+                  className="flex items-center space-x-2 focus:outline-none"
                 >
-                  Logout
-                </a>
-              </button>
+                  <Image
+                    src="/avatar_placeholder.png"
+                    alt="Avatar"
+                    width={45}
+                    height={0}
+                    className="rounded-full"
+                  />
+                </button>
+                {dropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-auto bg-white rounded-md shadow-lg py-2 z-20">
+                    <span className="block font-bold px-4 py-2 text-gray-800">
+                      Hola, {session.user.email}
+                    </span>
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        signOut({
+                          redirect: true,
+                          callbackUrl:
+                            process.env.NEXT_PUBLIC_FRONTEND_BASE_URL,
+                        });
+                      }}
+                      className="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    >
+                      Salir
+                    </button>
+                  </div>
+                )}
+              </div>
             </>
           ) : (
-            <>
-              <Link
-                className="text-white bg-blue-500 px-3 py-2 rounded-md"
-                href="/login"
-              >
-                Login
-              </Link>
-            </>
+            <Link
+              className="text-white bg-blue-500 px-3 py-2 rounded-md"
+              href="/login"
+            >
+              Login
+            </Link>
           )}
         </div>
       </div>

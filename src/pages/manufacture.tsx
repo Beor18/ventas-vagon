535e21af13d53a3367faded8b6f5de894338cef9
Fernando Lopez <fer.ragnar19@gmail.com>
2024-07-24 09:04:22 -0300

    fix text

--- src/pages/manufacture.tsx
+++ src/pages/manufacture.tsx
@@ -186,7 +186,7 @@ function Manufacture() {
                 className="bg-red-500 text-white px-4 py-2 rounded-md"
                 onClick={() => setShowCommentInput(true)}
               >
-                Rechazar
+                Cancelar orden
               </button>
             </div>
             {showCommentInput && (
@@ -201,7 +201,7 @@ function Manufacture() {
                   className="bg-red-500 text-white px-4 py-2 rounded-md mt-2"
                   onClick={() => rejectOrder(selectedOrder._id)}
                 >
-                  Confirmar Rechazo
+                  Confirmar
                 </button>
               </div>
             )}

e85a4d1917700fe3fcff0c30ff5f881836f3007f
Fernando Lopez <fer.ragnar19@gmail.com>
2024-10-24 11:09:08 -0300

    fix style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
-import { connectToDatabase } from "../lib/mongodb";
-import Product from "../models/Product";
-import Modal from "@/components/Modal";
-import Select from "@/components/Select";
+/* eslint-disable jsx-a11y/alt-text */
+import Image from "next/image";
 
 export default function Home({ products }: any) {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
-      <div className="container mx-auto p-4 flex-grow"></div>
+      <div className="container mx-auto p-4 flex-grow flex items-center justify-center">
+        <div className="relative w-full max-w-[500px] aspect-square">
+          <Image
+            src="/vagon_5_Transparent.png"
+            alt="Centered wagon image"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </div>
     </div>
   );
 }

32911d6ce889a3934db49a5c96e66aec1306cd99
Fernando Lopez <fer.ragnar19@gmail.com>
2024-12-02 15:27:02 -0300

    add create client

--- src/components/Seller/ClientTable/index.tsx
+++ src/components/Seller/ClientTable/index.tsx
@@ -21,52 +21,59 @@ export default function ClientsTable({
   handleDeleteClient,
 }: any) {
   return (
-    <div className="rounded-md border">
-      <Button onClick={() => openClientFormModal()} size="lg">
-        <PlusCircle className="mr-2 h-5 w-5" /> Crear cliente
-      </Button>
-      <Table>
-        <TableHeader>
-          <TableRow className="bg-muted/50">
-            <TableHead>Nombre</TableHead>
-            <TableHead>Email</TableHead>
-            <TableHead>Teléfono</TableHead>
-            <TableHead>Dirección</TableHead>
-            <TableHead className="text-right">Acciones</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {clients.map((client: any) => (
-            <TableRow key={client._id}>
-              <TableCell>{client.nombre}</TableCell>
-              <TableCell>{client.email}</TableCell>
-              <TableCell>{client.telefono}</TableCell>
-              <TableCell>{client.direccion_residencial}</TableCell>
-              <TableCell className="text-right">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="h-8 w-8 p-0">
-                      <MoreVertical className="h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuItem
-                      onClick={() => openClientFormModal(client)}
-                    >
-                      Editar
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={() => handleDeleteClient(client._id)}
-                    >
-                      Eliminar
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
+    <div>
+      <div className="flex justify-between items-center mb-8">
+        <h2 className="text-3xl font-bold">
+          Lista clientes ({clients.length})
+        </h2>
+        <Button onClick={() => openClientFormModal()} size="lg">
+          <PlusCircle className="mr-2 h-5 w-5" /> Crear cliente
+        </Button>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow className="bg-muted/50">
+              <TableHead>Nombre</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Teléfono</TableHead>
+              <TableHead>Dirección</TableHead>
+              <TableHead className="text-right">Acciones</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {clients.map((client: any) => (
+              <TableRow key={client._id}>
+                <TableCell>{client.nombre}</TableCell>
+                <TableCell>{client.email}</TableCell>
+                <TableCell>{client.telefono}</TableCell>
+                <TableCell>{client.direccion_residencial}</TableCell>
+                <TableCell className="text-right">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="ghost" className="h-8 w-8 p-0">
+                        <MoreVertical className="h-4 w-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuItem
+                        onClick={() => openClientFormModal(client)}
+                      >
+                        Editar
+                      </DropdownMenuItem>
+                      <DropdownMenuItem
+                        onClick={() => handleDeleteClient(client._id)}
+                      >
+                        Eliminar
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
